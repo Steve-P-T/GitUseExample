4fffaf639de0252a174b61a49e15d15ed513e96e
Steve-P-T <stevetweedale@icloud.com>
2020-04-02 13:02:28 +0100

    Add map for itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Steve Tweedale on 02/04/2020.
+//  Copyright © 2020 Steve Tweedale. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSManagedObject {
+
+}
